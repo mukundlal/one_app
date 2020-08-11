527dbcf87096ebce2816d9fad3b5401d8c17e60d
mukundlal <mukundlaltnr@gmail.com>
2020-08-12 01:26:30 +0530

    some small changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(
-    appBar: AppBar(
-      title: Text("Home",),
-      centerTitle: true,
-      backgroundColor: Colors.lightGreen,
-    ),
-  body: Center(child: Text("hello",style: TextStyle(color: Colors.green,fontSize: 20,fontWeight: FontWeight.bold)),),
-    floatingActionButton: FloatingActionButton(child: Text('click'),backgroundColor: Colors.green,),
-  ),));
+  runApp(MaterialApp(home: Home()),);
 }
 
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+  appBar: AppBar(
+    title: Text("Home",),
+    centerTitle: true,
+    backgroundColor: Colors.lightGreen,
+  ),
+  body: Center(child:Image(width: 500,height: 500,alignment: Alignment.center, image: NetworkImage("https://images.pexels.com/photos/4956618/pexels-photo-4956618.jpeg?auto=compress&cs=tinysrgb&h=650&w=940")),),
+  floatingActionButton: FloatingActionButton(child: Text('click'),backgroundColor: Colors.green,),
+  );
+  }
 
+}
 
